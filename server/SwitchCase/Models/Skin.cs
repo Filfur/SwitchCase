b2226ff43336b4671705d47e0f4d76353f712a72
zinya.play@gmail.com <zinya.play@gmail.com>
2019-11-24 22:53:16 +0300

    models created

---
+++ server/SwitchCase/Models/Skin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SwitchCase.Models
+{
+    public class Skin
+    {
+        public int SkinId { get; set; }
+
+        public int WeaponId { get; set; }
+
+        public string Name { get; set; }
+
+        public int CaseId { get; set; }
+
+        public string ImageLink { get; set; }
+
+        public float Price { get; set; }
+
+        public enum RarityEnum { MilSpec, Restricted, Classified, Covert }
+
+        public RarityEnum Rarity { get; set; }
+
+        public double DropRate { get; set; }
+    }
+}
