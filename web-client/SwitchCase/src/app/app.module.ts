044ddf18aa8842cb92955e9c71293687dbef422a
Haezar <mrcatby.mg@gmail.com>
2019-11-24 22:51:32 +0300

    Web: routing

--- web-client/SwitchCase/src/app/app.module.ts
+++ web-client/SwitchCase/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule} from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { CasesComponent } from './cases/cases.component';
 import { FooterComponent } from './footer/footer.component';
+import { DetailsComponent } from './details/details.component';
+
+const appRoutes: Routes = [
+  {path: '', component: CasesComponent},
+  {path: 'details', component: DetailsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -13,10 +19,11 @@ import { FooterComponent } from './footer/footer.component';
     HeaderComponent,
     CarouselComponent,
     CasesComponent,
-    FooterComponent
+    FooterComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
